ae7983281d6077915f97dc732ad57ac2c9fd3a37
ashish0038 <ashish.0038@gmail.com>
2024-05-19 12:25:28 +0530

    updated review

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -436,3 +436,16 @@ var leafContainer = document.querySelector('.falling-leaves'),
 
 leaves.init();
 leaves.render();
+
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
+
